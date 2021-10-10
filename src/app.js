d77ab4a33d6af5f2ca195e3ef4ecea61fb1a7dd5
ismabk <ismabk94@gmail.com>
2021-10-10 15:15:12 +0000

    Finalizado generador de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
+
+let generateExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  var excusa = "";
+  excusa += who[Math.floor(Math.random() * (who.length - 1) + 1)] + " ";
+  excusa += what[Math.floor(Math.random() * (what.length - 1) + 1)] + " ";
+  excusa += when[Math.floor(Math.random() * (when.length - 1) + 1)] + " ";
+  console.log(excusa);
+  return excusa;
 };
